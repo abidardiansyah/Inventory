7c2d506928cf643e953a8d3bdef0e48e7f67f88c
abidardiansyah <abidardiansyah1@gmail.com>
2023-07-11 00:16:18 +0700

    first

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,81 @@
+<header class="main-header ">
+    <!-- Logo -->
+   
+        <div class="navinfo">
+    <a href="index2.html" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        @php
+            $words = explode(' ', $setting->nama_perusahaan);
+            $word  = '';
+            foreach ($words as $w) {
+                $word .= $w[0];
+            }
+        @endphp
+        <span class="logo-mini">{{ $word }}</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg ">
+            <img src="/img/logo-20230709162620.jpg" >
+            {{-- <b>{{ $setting->nama_perusahaan }}</b> --}}
+            <b>TB Linggajaya</b>
+        </span>
+    </a>
+</div>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top ">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+           <style>
+            .user-info {
+                display: flex;
+                align-items: center;
+                }
+
+                .user-info .hidden-xs {
+                margin-right: 10px;
+                
+                }
+                </style>        
+                   <a href="#" class="dropdown-toggle " data-toggle="dropdown">
+                      
+                            <span class=" hidden-xs">{{ auth()->user()->name }}</span>
+                            <img src="{{ url(auth()->user()->foto ?? '') }}" class="user-image img-profil" alt="User Image">
+                        
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <img src="{{ url(auth()->user()->foto ?? '') }}" class="img-circle img-profil"
+                                alt="User Image">
+
+                            <p>
+                                {{ auth()->user()->name }} - {{ auth()->user()->email }}
+                            </p>
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="{{ route('user.profil') }}" class="btn btn-default btn-flat">Profil</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="#" class="btn btn-default btn-flat"
+                                    onclick="$('#logout-form').submit()">Keluar</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<form action="{{ route('logout') }}" method="post" id="logout-form" style="display: none;">
+    @csrf
+</form>
